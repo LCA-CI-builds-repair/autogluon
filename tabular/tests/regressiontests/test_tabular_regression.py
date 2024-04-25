b723b47faa53c95b017167ee8f703033e4093a11
ubuntu <ubuntu@ip-10-0-0-80>
2024-04-25 23:48:40 +0000

    test_lca_code_editing__code-editing-metrics-275068529e1a3144__id_122

--- tabular/tests/regressiontests/test_tabular_regression.py
+++ tabular/tests/regressiontests/test_tabular_regression.py
@@ -69,7 +69,7 @@ def inner_test_tabular(testname):
     # Now run the Predictor 1 or more times with various parameters, and make sure we get
     # back the expected results.
 
-    # Params can either omitted, or a single run, or a list of runs.
+    # Params can either be omitted, or a single run, or a list of runs.
     if "params" not in test:
         test["params"] = {"predict": {}, "fit": {}}
     if not isinstance(test["params"], list):
