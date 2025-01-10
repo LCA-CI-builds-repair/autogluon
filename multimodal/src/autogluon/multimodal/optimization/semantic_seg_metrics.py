481e79476ce29c617ca9c047170e893f17b1c197
ubuntu <ubuntu@ip-10-0-1-21>
2025-01-10 12:20:25 +0000

    waleko__claude-3.5-sonnet_d33e__id_121

--- multimodal/src/autogluon/multimodal/optimization/semantic_seg_metrics.py
+++ multimodal/src/autogluon/multimodal/optimization/semantic_seg_metrics.py
@@ -589,7 +589,7 @@ class Multiclass_IoU(torchmetrics.Metric):
         self.total_inter += inter
         self.total_union += union
 
-    def compute(self):
+    def compute_evaluation(self):(self):
         IoU = 1.0 * self.total_inter / (2.220446049250313e-16 + self.total_union)
         return torch.tensor(IoU.mean().item())
 
